f7dcde175ba30c2f58139ea5d2b8e9ab1dda5511
Joseph Abrahamson <me@jspha.com>
2015-02-25 23:49:43 -0500

    Void is a useful type to put somewhere

---
+++ src/types/void.ml
@@ -0,0 +1,7 @@
+
+type t = { t : 'a . 'a }
+(* The absurdity of [t] is self-evident. The only "natural" way to
+   produce a value of [t] is to have a value of literally any type
+   with no constraint. *)
+
+let absurd t = t.t
---
+++ src/types/void.mli
@@ -0,0 +1,21 @@
+(** The impossible type: no values can be made to exist.
+
+    Values of type {!t} cannot be made to exist, there are no
+    introduction forms. This makes them the impossible type; however,
+    they are not without their use. A function which claims to return
+    {!t} must actually never return. A hypothetical situation which
+    offers a value of {!t} must be {!absurd}.
+
+*)
+
+type t
+(** The nonexistent data type. *)
+
+val absurd : t -> 'a
+(** It is possible in some hypothetical contexts to have access to a
+    value of type {!t}. As no values of {!t} can ever come into
+    existence we resolve that our hypotheses are wrong and thus
+    conclude whatever we like.
+
+    In pithier words, {i from nothing comes anything}.
+*)
